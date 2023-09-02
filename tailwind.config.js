5224e60262fcbd900b8b142b85b9032a50f1ee28
Steeven <82022828+steeven-th@users.noreply.github.com>
2023-09-02 13:52:16 +0200

    préparation du socle symfony / mise en place de webpack + tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
